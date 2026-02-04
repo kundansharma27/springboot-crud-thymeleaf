fbb342c0e2ff9550b32b4c9f63f87105a7c4036b
kundansharma2712-pixel <kundansharma2712@gmail.com>
2026-02-04 22:35:53 +0530

    Implement create product feature with DTO and validation

--- src/main/java/com/boostmytool/beststore/controllers/ProductsController.java
+++ src/main/java/com/boostmytool/beststore/controllers/ProductsController.java
@@ -1,8 +1,10 @@
 package com.boostmytool.beststore.controllers;
 
 import com.boostmytool.beststore.models.Product;
+import com.boostmytool.beststore.models.ProductDto;
 import com.boostmytool.beststore.services.ProductsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,9 +21,15 @@ public class ProductsController {
 
     @GetMapping({"","/"})
     public String showProductList(Model model) {
-        List<Product> products = repo.findAll();
+        List<Product> products = repo.findAll(Sort.by(Sort.Direction.DESC,"id"));
         model.addAttribute("products", products);
         return "products/index";
 
+    }
+    @GetMapping("/create")
+    public String showCreatePage(Model model) {
+        ProductDto productDto = new ProductDto();
+        model.addAttribute("productDto",productDto);
+        return "products/CreateProduct";
     }
 }
