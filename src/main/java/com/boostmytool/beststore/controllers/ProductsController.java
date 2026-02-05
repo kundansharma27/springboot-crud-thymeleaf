f92a0b1d28e5acdce17baaf509ef29f94cdeda85
kundansharma2712-pixel <kundansharma2712@gmail.com>
2026-02-05 22:18:19 +0530

    Add errors in create form.

--- src/main/java/com/boostmytool/beststore/controllers/ProductsController.java
+++ src/main/java/com/boostmytool/beststore/controllers/ProductsController.java
@@ -3,11 +3,16 @@ package com.boostmytool.beststore.controllers;
 import com.boostmytool.beststore.models.Product;
 import com.boostmytool.beststore.models.ProductDto;
 import com.boostmytool.beststore.services.ProductsRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -32,4 +37,17 @@ public class ProductsController {
         model.addAttribute("productDto",productDto);
         return "products/CreateProduct";
     }
+   @PostMapping("/create")
+    public String  createProduct(
+            @Valid @ModelAttribute ProductDto productDto,
+            BindingResult result
+   ) {
+        if (productDto.getImageFile().isEmpty()) {
+            result.addError(new FieldError("productDto","imageFile", "The image File is required"));
+            }
+        if (result.hasErrors()){
+            return "products/CreateProduct";
+        }
+        return "redirect:/products";
+   }
 }
